3ba40518b79d9362ec64e9e18e68b1ea9078837a
maratishe <maratishe@gmail.com>
2015-12-30 16:31:11 +0900

    upload

---
+++ example.php
@@ -0,0 +1,59 @@
+<?php
+$CLASS = 'example'; class example { 
+	public $iscli = true; // set to false in web mode
+	public function server( $iport = '127.0.0.1:8001') { system( "php -S $iport -t ."); } 
+	public function test() { die( jsonsend( jsonmsg( 'I am here!'))); }
+	
+	
+}
+// CLI fork
+if ( isset( $argv) && count( $argv) && strpos( $argv[ 0], "$CLASS.php") !== false) { // direct CLI execution, redirect to one of the functions 
+	// this is a standalone script, put the header
+	set_time_limit( 0);
+	ob_implicit_flush( 1);
+	for ( $prefix = is_dir( 'ajaxkit') ? 'ajaxkit/' : ''; ! is_dir( $prefix) && count( explode( '/', $prefix)) < 4; $prefix .= '../'); if ( ! is_file( $prefix . "env.php")) $prefix = '/web/ajaxkit/'; if ( ! is_file( $prefix . "env.php")) die( "\nERROR! Cannot find env.php in [$prefix], check your environment! (maybe you need to go to ajaxkit first?)\n\n");
+	if ( is_file( 'requireme.php')) require_once( 'requireme.php'); else foreach ( array( 'functions', 'env') as $k) require_once( $prefix . "$k.php"); clinit(); 
+	foreach ( array( 'functions', 'env') as $k) if ( is_file( "$k.php")) require_once( "$k.php");
+	chdir( clgetdir()); clparse(); $JSONENCODER = 'jsonencode'; // jsonraw | jsonencode    -- jump to lib dir
+	// help
+	clhelp( "FORMAT: php$CLASS WDIR COMMAND param1 param2 param3...     ($CLNAME)");
+	foreach ( file( $CLNAME) as $line) if ( ( strpos( trim( $line), '// SECTION:') === 0 || strpos( trim( $line), 'public function') === 0) && strpos( $line, '__construct') === false) clhelp( trim( str_replace( 'public function', '', $line)));
+	// parse command line
+	lshift( $argv); if ( ! count( $argv)) die( clshowhelp()); 
+	//$wdir = lshift( $argv); if ( ! is_dir( $wdir)) { echo "ERROR! wdir#$wdir is not a directory\n\n"; clshowhelp(); die( ''); }
+	//echo "wdir#$wdir\n"; if ( ! count( $argv)) { echo "ERROR! no action after wdir!\n\n"; clshowhelp(); die( ''); }
+	$f = lshift( $argv); $C = new $CLASS( true); chdir( $CWD); 
+	switch ( count( $argv)) { case 0: $C->$f(); break; case 1: $C->$f( $argv[ 0]); break; case 2: $C->$f( $argv[ 0], $argv[ 1]); break; case 3: $C->$f( $argv[ 0], $argv[ 1], $argv[ 2]); break; case 4: $C->$f( $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3]); break; case 5: $C->$f( $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3], $argv[ 4]); break; case 6: $C->$f( $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3], $argv[ 4], $argv[ 5]); break; }
+ 	//switch ( count( $argv)) { case 0: $C->$f( $wdir); break; case 1: $C->$f( $wdir, $argv[ 0]); break; case 2: $C->$f( $wdir, $argv[ 0], $argv[ 1]); break; case 3: $C->$f( $wdir, $argv[ 0], $argv[ 1], $argv[ 2]); break; case 4: $C->$f( $wdir, $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3]); break; case 5: $C->$f( $wdir, $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3], $argv[ 4]); break; case 6: $C->$f( $wdir, $argv[ 0], $argv[ 1], $argv[ 2], $argv[ 3], $argv[ 4], $argv[ 5]); break; }
+ 	die();
+}
+if ( ! isset( $argv) && ( isset( $_GET) || isset( $_POST)) && ( $_GET || $_POST)) { // web API 
+	set_time_limit( 0);
+	ob_implicit_flush( 1);
+	$prefix = ''; if ( is_dir( "ajaxkit")) $prefix = 'ajaxkit/'; for ( $i = 0; $i < 3; $i++) { if ( ! is_dir( $prefix . 'lib')) $prefix .= '../'; else break; }
+	if ( ! is_file( $prefix . "env.php")) $prefix = '/web/ajaxkit/'; // hoping for another location of ajaxkit
+	if ( ! is_file( $prefix . "env.php")) die( "\nERROR! Cannot find env.php in [$prefix], check your environment! (maybe you need to go to ajaxkit first?)\n\n");
+	// global functions and env
+	require_once( $prefix . 'functions.php');
+	require_once( $prefix . 'env.php'); //echo "env[" . htt( $env) . "]\n";
+	// additional (local) functions and env (if present)
+	if ( is_file( "$BDIR/functions.php")) require_once( "$BDIR/functions.php");
+	if ( is_file( "$BDIR/env.php")) require_once( "$BDIR/env.php");
+	htg( hm( $_GET, $_POST)); 
+	// check for webkey.json and webkey parameter in request
+	//if ( ! is_file( 'webkey.json') || ! isset( $webkey)) die( jsonsend( jsonerr( 'webkey env not set, run [phpwebkey make] first'))); 
+	//$h = jsonload( 'webkey.json'); if ( ! isset( $h[ "$webkey"])) die( jsonsend( jsonerr( 'no such webkey in your current environment')));
+	//$wdir = $h[ "$webkey"]; if ( ! is_dir( "$wdir")) die( jsonsend( jsonerr( "no dir $wdir in local filesystem, webkey env is wrong")));
+	// actions: [wdir] is fixed/predefined  [action] is function name   others are [one,two,three,...]
+	$O = new $CLASS( false); $O->iscli = false; // does not pass [types], expects the user to run init() once locally before using it remotely 
+	$p = array(); foreach ( ttl( 'one,two,three,four,five') as $k) if ( isset( $$k)) lpush( $p, $$k); $R = array();
+	if ( count( $p) == 0) $R = $O->$action();
+	if ( count( $p) == 1) $R = $O->$action( $one);
+	if ( count( $p) == 2) $R = $O->$action( $one, $two);
+	if ( count( $p) == 3) $R = $O->$action( $one, $two, $three);
+	if ( count( $p) == 4) $R = $O->$action( $one, $two, $three, $four);
+	if ( count( $p) == 5) $R = $O->$action( $one, $two, $three, $four, $five);
+	die( jsonsend( $R));
+}
+if ( isset( $argv) && count( $argv)) { $L = explode( '/', $argv[ 0]); array_pop( $L); if ( count( $L)) chdir( implode( '/', $L)); } // WARNING! Some external callers may not like you jumping to current directory
+?>
